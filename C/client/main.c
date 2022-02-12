e637489708bb38677c676be832f3808808b90a6f
Mazene-ZERGUINE <mmazenezerguine@gmail.com>
2022-02-12 15:44:26 +0100

    create/send/show

--- C/client/main.c
+++ C/client/main.c
@@ -5,7 +5,6 @@
 #include <string.h>
 
 
-
 /* declaration des variables GTK */
 
 GtkBuilder *builder ;
@@ -23,7 +22,7 @@ GtkWidget *localEntry2 ;
 GtkWidget *sendButton2 ;
 
 
-/* fonction de l'affichage de la fentre des erreur */
+/* *************************fonction de l'affichage de la fentre des erreur *******************************/
 
 void createErrorWindow(int argc , char **argv) {
   gtk_init(&argc , &argv) ;                                                  /* initialisation GTK */
@@ -45,7 +44,7 @@ void createErrorWindow(int argc , char **argv) {
 }
 
 
-/* fonction de l'ajout d'une tahces au fichier yaml*/
+/******************************** fonction de l'ajout d'une tahces au fichier yaml******************************/
 
 void on_addButton_clicked(int argc , char **argv) {
 char date[12] ;
@@ -53,6 +52,7 @@ char local[12] ;
 char task[255] ;
 FILE *yaml ;
 char fileName[255] ;
+int status = 1 ;
 
 
 /* récupération des valeur des entrés GTK */
@@ -72,15 +72,27 @@ if(yaml == NULL) {
 
 } else {
 
-  if (strlen(date) == 0 || strlen(task) == 0 || strlen(local) == 0) {
+  if (strlen(date) == 0 || strlen(task) == 0 || strlen(local) == 0) {   /*en cas d'un champs vide affichage d'une fentre qui demande à l'utilisater de remplire tous les champs*/
 
-    createErrorWindow(&argc , &argv) ;                            /*en cas d'un champs vide affichage d'une fentre qui demande à l'utilisater de remplire tous les champs*/
+    createErrorWindow(&argc , &argv) ;
 
     gtk_label_set_text(GTK_LABEL(errorLabel) , "merci de remplire tous les champs avant d'ajouter  ") ;
 
     gtk_main() ;
 
+    status = 0 ;                                     /* en cas de champs non rempli en mis la valeur de verification a 0 */
+
   }
+  if(strlen(date) != 10 ) {                                                 /* verification de la valeur du date entrer 'format dd/mm/yyyy'*/
+    createErrorWindow(&argc , &argv) ;
+
+    gtk_label_set_text(GTK_LABEL(errorLabel) , "                         date éroné : \n entrer un date au format (dd/mm/yyyy) ") ;
+
+    gtk_main() ;
+
+    status = 0 ;
+  }
+  if(status == 1) {                                /* on execute cette partie du conde uniquement si tous les champs sont bien remplie */
   char *tmp ;                                      /* ecréture des donnée dans le fichier yaml */
   tmp = malloc(255) ;
   strcat(strcpy(tmp , "- date: ") , date) ;           //- date: date
@@ -100,19 +112,17 @@ if(yaml == NULL) {
 
 
 }
+}
 fclose(yaml) ;                          /* ferméture de fichier yaml */
 
 
 
 }
 
-/*fonction d'envoi de fichier yaml */
+/*********************************************** selection d'entrepot d'envoie ******************************************/
 
 void on_sendButton_clicked(int argc , char **argv) {
-
-                              /*affcihage d'une fenetre qui demande à l'utilisater de séléction l'entrepot concerné afin de pouvoir
-                                                        ouvrire le bon fichier yaml
-                                                                                                                                  */
+                              /*affcihage d'une fenetre qui demande à l'utilisater de séléction l'entre*/
 
   char local[255] ;
 
@@ -134,15 +144,115 @@ void on_sendButton_clicked(int argc , char **argv) {
     gtk_main() ;
 
 }
-/*foncrion d'envoie du fichier yaml en ftp*/
+/***************************************** fonction d'envoie du fichier yaml **********************************************/
+
 void on_sendButton2_clicked(int argc , char **argv) {
+  char local[255] ;
+  char filePath[255] ;
+  FILE *file ;
+  CURL *curl;
+  CURLcode res;
+  char url[255] ;
 
 
+  sprintf(local , "%s" , gtk_entry_get_text(GTK_ENTRY(localEntry2))) ;                        /* récupération de la valeur de l'entré gtk */
+
+  strcat(strcpy(filePath , local),".yaml");
+  strcat(strcpy(url , "ftp://mazene:1234@172.16.57.128/") , filePath) ;
+
+  struct curl_slist *headerlist = NULL ;
+
+  file = fopen(filePath , "rb") ;                                                          /* ouverture du fichier a envoyer */
+
+  if (file == NULL ) {                                                                     /* verification si le fichier n'existe pas */
+
+    gtk_window_close (selectWindow);
+
+    createErrorWindow(&argc , &argv) ;                            /*en cas d'un champs vide affichage d'une fentre qui demande à l'utilisater de remplire tous les champs*/
+
+    gtk_label_set_text(GTK_LABEL(errorLabel) , "                       aucunne tache n'est entré               ") ;
+
+
+  }  else {
+
+
+    curl_global_init(CURL_GLOBAL_ALL);                                      /* initialisation de curl */
+    curl = curl_easy_init();
+
+    headerlist = curl_slist_append(headerlist, filePath);                  /*liste des header curl <nom du fichier a envoyer / nom du fichier aprés l'evoie >*/
+    headerlist = curl_slist_append(headerlist, filePath);
+
+    curl_easy_setopt(curl, CURLOPT_READFUNCTION, fread);                 /* lecture du fichier yaml */
+
+    curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);                           /* activation de la fonctionalité upload*/
+    curl_easy_setopt(curl, CURLOPT_URL, url);                             /*on définit le serveur ou on veut envoyer le fichier*/
+    curl_easy_setopt(curl, CURLOPT_POSTQUOTE, headerlist);                /* on applic la liste des headers */
+    curl_easy_setopt(curl, CURLOPT_READDATA, file);
+    res = curl_easy_perform(curl);                                       /* on execute la commande curl */
+
+    if(res != CURLE_OK)
+      fprintf(stderr, "curl_easy_perform() failed: %s\n",               /* verification d'erreur */
+              curl_easy_strerror(res));
+
+
+    curl_slist_free_all(headerlist);                                       /*free de la liste des headers */
+
+
+    curl_easy_cleanup(curl);                                              /* free de curl */
+
+
+  fclose(file); /* close the local file */                              /* ferméture du fichier */
+
+  curl_global_cleanup();
+
+  gtk_window_close (selectWindow);                                  /* affichage d'une fentre avec un message de success */
+
+  createErrorWindow(&argc , &argv) ;
+
+  gtk_label_set_text(GTK_LABEL(errorLabel) , "                       fichier envoyer              ") ;
+
+
+  gtk_main() ;
 }
 
 
 
-                                                              /* fonction main du programme */
+  }
+
+void on_showButton_clicked() {
+
+
+  CURL *curl;
+  FILE *file;
+  CURLcode res;
+  char url[] = "172.16.57.128/home/mazene/home.xls" ;
+
+
+     curl = curl_easy_init();         /* initialisation de curl*/
+     if (curl)
+     {
+         file = fopen("new.xls","wb");                           /*ouvertur de fichier fileName mode ecriture */
+         curl_easy_setopt(curl, CURLOPT_URL, url);
+         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, NULL);   /* suivi d'url */
+         curl_easy_setopt(curl, CURLOPT_WRITEDATA, file);        /*on ecrit les data récupérer dans le fichier fileName */
+         res = curl_easy_perform(curl);                     /*execution de la commande curl*/
+         curl_easy_cleanup(curl);              /*free*/
+         fclose(file);                                        /*ferméture de fichier */
+         printf("téléchargement terminer \n") ;
+
+         system("open new.xls") ; 
+
+     }   else {
+
+         fprintf(stderr, "curl_easy_perform() failed: %s\n",   /* verification erreur */
+              curl_easy_strerror(res));
+
+     }
+
+}
+
+
+/***************************************** fonction main du programme ****************************************/
 
 int main(int argc , char **argv) {
 
