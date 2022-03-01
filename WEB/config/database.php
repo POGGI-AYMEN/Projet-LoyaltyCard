e629911df3cad07b42a41ba7240ef1f56251bf0f
Mazene-ZERGUINE <mmazenezerguine@gmail.com>
2022-03-01 10:37:25 +0100

    WEB : inscreption/connexion/mot_de_passe

--- WEB/config/database.php
+++ WEB/config/database.php
@@ -5,7 +5,7 @@ $password = "" ;
 
 $con = new PDO ('mysql:host=localhost;dbname=PA', $username , $password);
 
-if (!con) {   /* on cas d'echec de la connexion avec la base de donnée */
+if (!$con) {   /* on cas d'echec de la connexion avec la base de donnée */
     header('location:error.php?message=impossible de se connecter à la base de donnée ') ; 
 }
 
--- WEB/error.php
+++ WEB/error.php
@@ -1,132 +1,132 @@
 <!DOCTYPE html>
-<html lang="fr">
+
+<html lang="en">
 
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 
-	<title>404 HTML Tempalte by Colorlib</title>
+<title>Erreur</title>
+<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,900" rel="stylesheet">
 
-
-	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,900" rel="stylesheet">
-
+<link type="text/css" rel="stylesheet" href="css/style.css" />
 </head>
 
 <body>
 
-  <style>
-  * {
-    -webkit-box-sizing: border-box;
-            box-sizing: border-box;
-  }
+<style>
 
-  body {
-    padding: 0;
-    margin: 0;
-  }
+* {
+  -webkit-box-sizing: border-box;
+          box-sizing: border-box;
+}
 
-  #notfound {
-    position: relative;
-    height: 100vh;
-  }
+body {
+  padding: 0;
+  margin: 0;
+}
 
-  #notfound .notfound {
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    -webkit-transform: translate(-50%, -50%);
-        -ms-transform: translate(-50%, -50%);
-            transform: translate(-50%, -50%);
-  }
+#notfound {
+  position: relative;
+  height: 100vh;
+}
 
-  .notfound {
-    max-width: 410px;
-    width: 100%;
-    text-align: center;
-  }
+#notfound .notfound {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  -webkit-transform: translate(-50%, -50%);
+      -ms-transform: translate(-50%, -50%);
+          transform: translate(-50%, -50%);
+}
 
-  .notfound .notfound-404 {
-    height: 280px;
-    position: relative;
-    z-index: -1;
-  }
+.notfound {
+  max-width: 410px;
+  width: 100%;
+  text-align: center;
+}
 
-  .notfound .notfound-404 h1 {
-    font-family: 'Montserrat', sans-serif;
-    font-size: 230px;
-    margin: 0px;
-    font-weight: 900;
-    position: absolute;
-    left: 50%;
-    -webkit-transform: translateX(-50%);
-        -ms-transform: translateX(-50%);
-            transform: translateX(-50%);
-    background: url('images/bg.jpg') no-repeat;
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
-    background-size: cover;
-    background-position: center;
-  }
+.notfound .notfound-404 {
+  height: 280px;
+  position: relative;
+  z-index: -1;
+}
 
-
-  .notfound h2 {
-    font-family: 'Montserrat', sans-serif;
-    color: #000;
-    font-size: 24px;
-    font-weight: 700;
-    text-transform: uppercase;
-    margin-top: 0;
-  }
-
-  .notfound p {
-    font-family: 'Montserrat', sans-serif;
-    color: #000;
-    font-size: 14px;
-    font-weight: 400;
-    margin-bottom: 20px;
-    margin-top: 0px;
-  }
-
-  .notfound a {
-    font-family: 'Montserrat', sans-serif;
-    font-size: 14px;
-    text-decoration: none;
-    text-transform: uppercase;
-    background: #0046d5;
-    display: inline-block;
-    padding: 15px 30px;
-    border-radius: 40px;
-    color: #fff;
-    font-weight: 700;
-    -webkit-box-shadow: 0px 4px 15px -5px #0046d5;
-            box-shadow: 0px 4px 15px -5px #0046d5;
-  }
+.notfound .notfound-404 h1 {
+  font-family: 'Montserrat', sans-serif;
+  font-size: 230px;
+  margin: 0px;
+  font-weight: 900;
+  position: absolute;
+  left: 50%;
+  -webkit-transform: translateX(-50%);
+      -ms-transform: translateX(-50%);
+          transform: translateX(-50%);
+  background: url('../img/bg.jpg') no-repeat;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-size: cover;
+  background-position: center;
+}
 
 
-  @media only screen and (max-width: 767px) {
-      .notfound .notfound-404 {
-        height: 142px;
-      }
-      .notfound .notfound-404 h1 {
-        font-size: 112px;
-      }
-  }
-  </style>
+.notfound h2 {
+  font-family: 'Montserrat', sans-serif;
+  color: #000;
+  font-size: 24px;
+  font-weight: 700;
+  text-transform: uppercase;
+  margin-top: 0;
+}
 
-	<div id="notfound">
-		<div class="notfound">
-			<div class="notfound-404">
-				<h1>Oops!</h1>
-			</div>
-			<h2><?php if (isset($_GET['message'])) echo $_GET['message'];  ?></h2>
-			<p>Veuillez essayer plus tard si le probleme persiste contacter </p> <a href="mailto:loyaltyCard@gmail.com">loyaltyCard@gmail.com</a>
-      <br>
-      <br>
-			<a href="index.php">Page d'accueil</a>
-		</div>
-	</div>
+.notfound p {
+  font-family: 'Montserrat', sans-serif;
+  color: #000;
+  font-size: 14px;
+  font-weight: 400;
+  margin-bottom: 20px;
+  margin-top: 0px;
+}
 
-</body><
+.notfound a {
+  font-family: 'Montserrat', sans-serif;
+  font-size: 14px;
+  text-decoration: none;
+  text-transform: uppercase;
+  background: #0046d5;
+  display: inline-block;
+  padding: 15px 30px;
+  border-radius: 40px;
+  color: #fff;
+  font-weight: 700;
+  -webkit-box-shadow: 0px 4px 15px -5px #0046d5;
+          box-shadow: 0px 4px 15px -5px #0046d5;
+}
+
+
+@media only screen and (max-width: 767px) {
+    .notfound .notfound-404 {
+      height: 142px;
+    }
+    .notfound .notfound-404 h1 {
+      font-size: 112px;
+    }
+}
+</style>
+
+<div id="notfound">
+<div class="notfound">
+<div class="notfound-404">
+<h1>Oops!</h1>
+</div>
+<h2> <?php if (isset($_GET['message']) && !empty($_GET['message'])) echo $_GET['message'] ; ?></h2>
+<p>Esseyer plus tard si ce probleme persiste contacter</p> <a href="mailto:loyaltyBoost@gmail.com">loyaltyBoost@gmail.com</a> 
+<a href="index.php">Retourner en page d'accueil</a>
+</div>
+</div>
+</body>
 
 </html>
+
+
