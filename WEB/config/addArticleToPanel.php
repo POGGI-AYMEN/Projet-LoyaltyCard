b875b881b384ed8b86b7c4ff3d78871719ee8d73
Mazene-ZERGUINE <mmazenezerguine@gmail.com>
2022-03-24 15:12:57 +0100

    update

--- WEB/config/addArticleToPanel.php
+++ WEB/config/addArticleToPanel.php
@@ -1,4 +1,58 @@
 <?php
+session_start() ; 
+
+include 'database.php' ; 
+
+$clientId = $_SESSION['clientId'] ; 
+$articleCode = $_GET['codeArticle'] ; 
+$quantity = $_GET['quantity'] ; 
+
+$sql = "SELECT * FROM Article WHERE codeArticle = ?"  ; 
+
+$query = $con->prepare($sql) ; 
+
+$query->execute([$articleCode]) ; 
+
+$result = $query->fetch(PDO::FETCH_ASSOC) ; 
+
+$articleName = $result['nom'] ; 
+$saler = $result['vendeur'] ; 
+$price = $result['prix'] ; 
+$category = $result['catégorie'] ; 
+
+$sql3 = "SELECT * FROM Panier WHERE article = ?" ; 
+
+$existQuery = $con->prepare($sql3) ; 
+
+$existQuery->execute([$articleName]) ;
+
+$exist = $existQuery->fetch(PDO::FETCH_ASSOC) ; 
+
+if (!empty($exist))   // verification si l'article est déja dans le panier
+{
+$existArticle = $exist['article'] ; 
+
+if (!empty($existArticle))         // si oui in incrémente quantité
+{
+  $updateQuery = $con->prepare("UPDATE Panier SET quantité = quantité + 1 WHERE article = ?") ; 
+  $updateQuery->execute([$existArticle]) ; 
+}
+}
+else          // sinon on l'ajoute dans le panier
+{
+$insertQuery = $con->prepare("INSERT INTO Panier (client , article, prix , catégorie, quantité ) VALUES (? , ? , ? , ?,?)") ; 
+
+$insertQuery->execute([
+     $clientId , 
+     $articleName ,
+     $price , 
+     $category,
+     $quantity 
+
+]) ; 
+
+}
 
 
-?>
+
+
--- WEB/panel.php
+++ WEB/panel.php
@@ -1,5 +1,53 @@
 <?php
+session_start() ; 
+
+include 'config/database.php' ; 
+
+if (isset($_SESSION['clientId'])) 
+{
+    $clientId = $_SESSION['clientId'] ; 
+    $selectQuery = $con->prepare("SELECT * FROM Panier WHERE client = ? ") ; 
+
+    $selectQuery->execute([$clientId]) ; 
+
+  
+    while($result = $selectQuery->fetch(PDO::FETCH_ASSOC))
+    {
+        $total = $result['prix'] * $result['quantité'] ; 
+       
+    ?>
+    <div>
+        <p>Article :  <?php echo $result['article']  ?></p>
+        <p> Catégorie : <?php echo $result['catégorie']  ?></p>
+
+        <p>Prix :<?php echo $result['prix']  ?></p>
+        <p>Quantité : <?php echo $result['quantité']  ?></p>
+
+        <p id="price"> totale : <?php echo $total   ?> </p>
 
 
+    </div>
+    <br>
+
+<?php
+} 
+
+}
+else
+{
+    header('location:connexion.php') ; 
+}
 
 ?>
+<p id="totalSum">somme total : </p>
+
+<form method="post">
+<input type="submit" name="paye" placeholder="payer">
+</form>
+
+
+<script>
+
+    sum = document.getElementById("price").innerHTML ; 
+    console.log(sum)
+</script>
